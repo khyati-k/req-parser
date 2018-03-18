d2e05dce87f3f939c4f40e80327485a7fe531da4
khyati-k <khyati.kul@gmail.com>
2018-03-18 17:58:03 +0400

    intial

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict'
+//dependencies
+const express = require('express');
+
+
+//global configuration
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/',(req,res) => {
+	const os = req.headers['user-agent'].match(/\(.*?\)/)[0]
+	const result = {'IP Address':req.hostname,
+	'language':req.headers['accept-language'].split(',')[0],
+	'OS':os.substr(1,os.length-2)
+	 }
+	res.send(result);
+});
+
+
+app.listen(PORT,() =>{
+	console.log('server is listening to %d port in %s mode', PORT,app.settings.env);
+});
